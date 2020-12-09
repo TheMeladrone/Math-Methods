ae0811119a58f69711020df8e8dd64aac3ba6312
TheMeladrone <50068950+TheMeladrone@users.noreply.github.com>
2020-12-09 20:37:50 +1100

    Add files via upload

---
+++ arccos.java
@@ -0,0 +1,18 @@
+class arccos {
+
+//Arccos definition. The arccosine function is the inverse function of cos(x). arccos(x) = cos-1(x) For example, If the cosine of 60° is 0.5: cos(60°) = 0.5.
+//Tip: acos(-1) returns the value of PI.
+
+public static void main(String[] args) {
+
+System.out.println(Math.acos(0.334));
+System.out.println(Math.acos(-0.43));
+System.out.println(Math.acos(1));
+System.out.println(Math.acos(3));
+System.out.println(Math.acos(-1));
+System.out.println(Math.acos(0)); 
+System.out.println(Math.acos(2));
+
+  }
+
+}
