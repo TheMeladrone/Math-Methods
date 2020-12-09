285c8795c21e29e21bed3ed8a5b0db4c4493389a
TheMeladrone <50068950+TheMeladrone@users.noreply.github.com>
2020-12-09 20:10:28 +1100

    Add files via upload

---
+++ abs.java
@@ -0,0 +1,22 @@
+class abs{
+
+public static void main(String[] args){
+     // get some integers to find their absolute values
+      int x = 9353;
+      double y = 33.3333;
+      int z = -1184;
+   
+      // get and print their absolute values
+      System.out.println("Math.abs(" + x + ")=" + Math.abs(x));   
+      System.out.println("Math.abs(" + y + ")=" + Math.abs(y));
+      System.out.println("This shows how positive numbers are printed correctly in the absolute value int or double");
+      System.out.println("");
+      System.out.println("This shows you dont even need to add to an int or set value you can just print abs(" + Math.abs(33)+ ")" );
+      System.out.println("");
+      System.out.println("Math.abs(" + z + ")=" + Math.abs(z));
+      System.out.println("This shows how negative numbers are made to be positive");
+      System.out.println("");
+   }
+}
+
+ 
